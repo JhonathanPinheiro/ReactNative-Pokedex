d55a0359787dae5a997766f95d9d073d6f93c7a4
Jhonathan Pinheiro <jhonathan@bemobile.tech>
2022-07-02 16:43:58 -0300

    GetPokeApi

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import theme from "./src/global/styles/theme";
+import { Home } from "./src/pages/Home";
 import { Welcome } from "./src/pages/Welcome";
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Welcome />
+      {/* <Welcome /> */}
+      <Home />
     </ThemeProvider>
   );
 }
