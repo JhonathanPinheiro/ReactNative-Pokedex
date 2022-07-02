dde1a4766012b5df088e57ce4b0186e2e4f1f2b1
Jhonathan Pinheiro <jhonathan@bemobile.tech>
2022-07-01 21:53:26 -0300

    Structure Theme Initial

---
+++ App.tsx
@@ -0,0 +1,6 @@
+import { ThemeProvider } from "styled-components";
+import theme from "./src/global/styles/theme";
+
+export default function App() {
+  return <ThemeProvider theme={theme}></ThemeProvider>;
+}
